e724ea8f2aff120707729020133572ba3f8af42f
Justin Yulli Kim <yulli@yulli.org>
2013-02-03 14:59:02 -0500

    Add hat-tip to 341 and summary of project

--- src/edu/stuy/EyeInThePi.java
+++ src/edu/stuy/EyeInThePi.java
@@ -4,6 +4,10 @@
  */
 package edu.stuy;
 
+/* Much of this code is based on Team 341's DaisyCV code.
+ * This is an implementation of their vision code using OpenCV on a Rasberry Pi.
+ * This runs as a standalone command-line utility that will run when the Pi on the robot starts.
+ */
 /**
  *
  * @author yulli
